2c4ed818c13db590cc224f8a953ea503aad2503a
arokux <arokux@gmail.com>
2013-10-22 22:44:34 +0200

    Revert "sunxi-usb: Add a helper function dbg_clocks(...)"
    This reverts commit 31cffd7a50e3469b6a5b99855c58d0591d422bc7.

--- drivers/usb/host/sw_hci_sunxi.c
+++ drivers/usb/host/sw_hci_sunxi.c
@@ -73,15 +73,6 @@ static u32 ohci_irq_no[3] = { 0, SW_INT_SRC_OHCI0, SW_INT_SRC_OHCI1 };
 static u32 usb1_set_vbus_cnt;
 static u32 usb2_set_vbus_cnt;
 
-
-static void dbg_clocks(struct sw_hci_hcd *sw_hci)
-{
-	DMSG_DEBUG("[%s]: clock info, SW_VA_CCM_AHBMOD_OFFSET(0x%x), SW_VA_CCM_USBCLK_OFFSET(0x%x)\n",
-		   sw_hci->hci_name,
-		   (u32) USBC_Readl(SW_VA_CCM_IO_BASE + SW_VA_CCM_AHBMOD_OFFSET),
-		   (u32) USBC_Readl(SW_VA_CCM_IO_BASE + SW_VA_CCM_USBCLK_OFFSET));
-}
-
 static s32 get_usb_cfg(struct sw_hci_hcd *sw_hci)
 {
 	__s32 ret = 0;
@@ -326,7 +317,10 @@ static int open_clock(struct sw_hci_hcd *sw_hci, u32 ohci)
 		     sw_hci->phy_reset, sw_hci->clk_is_open, sw_hci->ohci_gate);
 	}
 
-	dbg_clocks(sw_hci);
+	DMSG_DEBUG("[%s]: open clock, 0x60(0x%x), 0xcc(0x%x)\n",
+		   sw_hci->hci_name,
+		   (u32) USBC_Readl(SW_VA_CCM_IO_BASE + 0x60),
+		   (u32) USBC_Readl(SW_VA_CCM_IO_BASE + 0xcc));
 
 	return 0;
 }
@@ -355,7 +349,10 @@ static int close_clock(struct sw_hci_hcd *sw_hci, u32 ohci)
 		     sw_hci->phy_reset, sw_hci->clk_is_open, sw_hci->ohci_gate);
 	}
 
-	dbg_clocks(sw_hci);
+	DMSG_DEBUG("[%s]: close clock, 0x60(0x%x), 0xcc(0x%x)\n",
+		   sw_hci->hci_name,
+		   (u32) USBC_Readl(SW_VA_CCM_IO_BASE + 0x60),
+		   (u32) USBC_Readl(SW_VA_CCM_IO_BASE + 0xcc));
 
 	return 0;
 }
